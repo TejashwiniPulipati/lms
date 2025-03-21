d797b0a66ae0c9e1395b31142dec91a63e5ceb9b
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-21 22:14:35 +0530

    updated terraform module

--- terraform/modules/VPC/main.tf
+++ terraform/modules/VPC/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc" {
 # Web Subnet
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "var.public_subnet_cidr"
+  cidr_block = var.public_subnet_cidr
   availability_zone = var.availability_zone
   map_public_ip_on_launch = "true"
 
