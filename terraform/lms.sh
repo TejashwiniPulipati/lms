f17b36fd9341739299b9f8f204baf3429d21c97b
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-21 23:02:23 +0530

    updated lms.sh

--- terraform/lms.sh
+++ terraform/lms.sh
@@ -1,4 +1,6 @@
 # !/bin/bash
+sudo apt update -y
+sudo apt install zip unzip -y
 wget -O get-docker.sh https://get.docker.com && sh get-docker.sh
 sudo docker container run -dt --name sonarqube --restart=always -p 9000:9000 sonarqube
 sudo docker container run -dt --name nexus --restart=always -p 8081:8081 sonatype/nexus3
@@ -7,7 +9,17 @@ cd ~/lms/webapp
 npm install
 npm run build
 ls dist
-sudo apt -y install nginx
+sudo apt install nginx -y
+ls /var/www/html
+cat /var/www/html/index.nginx-debian.html
+sudo rm -rf /var/www/html/*
+sudo mkdir -p /var/www/html/
+ls /var/www/html
 sudo cp -r ~/lms/webapp/dist/* /var/www/html
+
+
+\
+
+
 
 
