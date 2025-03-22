de0ca6056e10efce0f15aaf254e64215a0d6df8d
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-22 11:36:20 +0530

    Update lms.sh

--- terraform/lms.sh
+++ terraform/lms.sh
@@ -3,7 +3,7 @@ wget -O get-docker.sh https://get.docker.com && sh get-docker.sh
 docker
 systemctl status docker
 sudo usermod -aG docker ubuntu
-exec ssh ubuntu@remote-server
+ssh ubuntu@remote-server
 docker container run -dt --name lms-fe -p 80:80 pulipatitejashwini/lms-fe:${APP_VERSION}
 
 
